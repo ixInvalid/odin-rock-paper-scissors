fa037ccab9604aa4277f68530e179401c9731835
ixInvalid <ixinvalidinbox+github@gmail.com>
2024-08-08 17:00:00 -0700

    added playRound function - logic to determined winner - return result message - update the scores

--- js/main.js
+++ js/main.js
@@ -25,3 +25,38 @@ function getComputerSelection() {
     const random = Math.floor(Math.random() * selection.length);
     return selection[random];
 }
+
+/*
+function getHumanSelection() {
+    let validSelection = ['ROCK', 'PAPER', 'SCISSORS'];
+    let selection = "";
+
+    while (!validSelection.includes(selection)) {
+        selection = prompt("Choose ROCK, PAPER, or SCISSORS:").toUpperCase();
+    }
+
+    return selection;
+}
+*/
+
+function playRound(humanSelection, computerSelection) {
+    const winningCombinations = {
+        ROCK: 'SCISSORS',
+        PAPER: 'ROCK',
+        SCISSORS: 'PAPER'
+    };
+
+    let result;
+
+    if (humanSelection === computerSelection) {
+        result = `You Tied! Human: ${humanSelection} Computer: ${computerSelection}`;
+    } else if (winningCombinations[humanSelection] === computerSelection) {
+        humanScore++;
+        result = `You Win! Human: ${humanSelection} Computer: ${computerSelection}`;
+    } else {
+        computerScore++;
+        result = `You Lose! Human: ${humanSelection} Computer: ${computerSelection}`;
+    }
+
+    return result
+}
