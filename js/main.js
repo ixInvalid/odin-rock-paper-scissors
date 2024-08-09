dda3f7c02e131bee6f10eb4256870153f2911ec0
ixInvalid <ixinvalidinbox+github@gmail.com>
2024-08-08 17:00:00 -0700

    added game selection and score tracking - created global variables     + humanScore     + computer score - fixed class naming - click event listeners - prevent game play if score is over 5 - invoke playGame function after click event listeners

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 let date = new Date().getFullYear();
 
 document.getElementById("year").innerHTML = `${date}`;
+
+let humanScore = 0;
+let computerScore = 0;
+
+const selection = Array.from(document.querySelectorAll('.select'));
+const message = document.getElementById('.message');
+const scorePlayer = document.querySelector('.user.score');
+const scoreComputer = document.querySelector('.computer.score');
+
+selection.forEach((box) =>
+    box.addEventListener('click', () => {
+        if (humanScore >= 5 || computerScore >= 5) {
+            return;
+        }
+        const humanSelection = box.classList[1].toUpperCase();
+        playGame(humanSelection);
+    })
+);
+
